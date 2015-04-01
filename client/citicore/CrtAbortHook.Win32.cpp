373a41eb886037969f76e1436c0f40aa59a9ea67
NTAuthority <bas@dotbas.net>
2015-04-02 00:18:21 +0200

    add hook for CRT DLL-related abort() calls (to aid debugging, if it's a Citizen-linked CRT component causing this particular target issue)

---
+++ client/citicore/CrtAbortHook.Win32.cpp
@@ -0,0 +1,23 @@
+/*
+ * This file is part of the CitizenFX project - http://citizen.re/
+ *
+ * See LICENSE and MENTIONS in the root of the source tree for information
+ * regarding licensing.
+ */
+
+#include "StdInc.h"
+#include <signal.h>
+
+static void HandleAbort(int signal)
+{
+	RaiseException(0xC000DEAD, 0, 0, nullptr);
+}
+
+static InitFunction initFunction([] ()
+{
+	signal(SIGABRT, HandleAbort);
+
+	_set_abort_behavior(0, _WRITE_ABORT_MSG);
+
+	abort();
+});
